074aa4aee9e2bb6d3e31b8a913f65de90d0a7fd0
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-11-15 16:28:06 +0300

    Updates

---
+++ precache-manifest.79d0b6eeae3d4e4526df62eb7c7969aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d5fd7c1c538c05e9ff0aec6e363a78",
+    "url": "/rgr2/index.html"
+  },
+  {
+    "revision": "d395bcf2d6cf0197c216",
+    "url": "/rgr2/static/css/2.610ef204.chunk.css"
+  },
+  {
+    "revision": "7da05e4b3512f9fa65ad",
+    "url": "/rgr2/static/css/main.bd091f6f.chunk.css"
+  },
+  {
+    "revision": "d395bcf2d6cf0197c216",
+    "url": "/rgr2/static/js/2.b3b93a62.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rgr2/static/js/2.b3b93a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da05e4b3512f9fa65ad",
+    "url": "/rgr2/static/js/main.bdf1e710.chunk.js"
+  },
+  {
+    "revision": "9bcf11353bd812674409",
+    "url": "/rgr2/static/js/runtime-main.038daa47.js"
+  },
+  {
+    "revision": "91f4475d007aa64dd9a0e79927f3d095",
+    "url": "/rgr2/static/media/ARIALUNI.91f4475d.TTF"
+  },
+  {
+    "revision": "09b1b6d3090b7c6491201c47488cd197",
+    "url": "/rgr2/static/media/graph_4_rgr2.09b1b6d3.jpg"
+  }
+]);
